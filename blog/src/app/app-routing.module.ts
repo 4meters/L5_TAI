64a5eeb35059441f67bae9ebb6871b2e4c6e5f33
Marcin <mryznar@onet.pl>
2021-03-22 23:25:05 +0100

    done

--- blog/src/app/app-routing.module.ts
+++ blog/src/app/app-routing.module.ts
@@ -1,7 +1,23 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+//const routes: Routes = [];
+
+const routes: Routes = [
+  {
+    path: 'home',
+    component: HomeComponent,
+  },
+  {
+    path: 'contact',
+    component: ContactComponent,
+  },
+  {
+    path: 'blog',
+component: BlogComponent,
+
+}];
+
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
